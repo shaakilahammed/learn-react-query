7d76af9bc14c9e84a7606ed21fc486f0de444f67
shaakilahammed <shaakilahammed@gmail.com>
2024-02-21 22:00:17 +0600

    setup: react, tailwind, @tanstack/react-query

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <h1 className="text-3xl text-center justify-center text-black font-extrabold italic">
+            Hello World
+        </h1>
+    );
+}
+
+export default App;
